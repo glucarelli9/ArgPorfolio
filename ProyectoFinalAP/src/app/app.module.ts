b239018fcfea9016225615ee9b57eec4afec74af
gian <glucarelli9@gmail.com>
2022-05-12 00:51:39 -0300

    v1.1

--- ProyectoFinalAP/src/app/app.module.ts
+++ ProyectoFinalAP/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExpComponent } from './components/exp/exp.component';
 import { EducComponent } from './components/educ/educ.component';
 import { HysComponent } from './components/hys/hys.component';
-
+import { HtmlParser } from '@angular/compiler';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,6 +30,17 @@ import { HysComponent } from './components/hys/hys.component';
     AppRoutingModule,
     NgbModule,
     NgCircleProgressModule.forRoot({
+      "backgroundPadding": 7,
+      "radius": 60,
+      "space": -2,
+      "outerStrokeWidth": 2,
+      "outerStrokeColor": "#8eb6c9",
+      "innerStrokeColor": "#e7e8ea",
+      "innerStrokeWidth": 2,
+      "animateTitle": false,
+      "animationDuration": 1000,
+      "showUnits": false,
+      "clockwise": false,
       
     })
   ],
