d5fbb5a645ae3a9c1e7b72534d5dda9d37e6aae3
gian <glucarelli9@gmail.com>
2022-05-12 00:00:46 -0300

    v1.0

--- ProyectoFinalAP/src/app/app.module.ts
+++ ProyectoFinalAP/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExpComponent } from './components/exp/exp.component';
+import { EducComponent } from './components/educ/educ.component';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExpComponent,
+    EducComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgCircleProgressModule.forRoot({
+      
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ProyectoFinalAP/src/app/components/educ/educ.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-educ',
+  templateUrl: './educ.component.html',
+  styleUrls: ['./educ.component.scss']
+})
+export class EducComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
